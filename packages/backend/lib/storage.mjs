1cb86d41f58ca5f979a526f9b6de18d654710e90
Markus Hauge <markus.hauge@hotmail.com>
2019-02-28 00:23:10 +0100

    Rename generateUrl to createShortUrl

--- packages/backend/lib/storage.mjs
+++ packages/backend/lib/storage.mjs
@@ -1,11 +1,11 @@
 import randomize from "randomatic";
 
 const storage = {};
-const generateUrl = key => `https://www.koble.jobs/${key}`;
+const createShortUrl = key => `https://www.koble.jobs/${key}`;
 
 export const longToShort = longUrl => {
   const key = randomize("Aa0", 6);
-  const shortUrl = generateUrl(key);
+  const shortUrl = createShortUrl(key);
   storage[shortUrl] = longUrl;
   return shortUrl;
 };
