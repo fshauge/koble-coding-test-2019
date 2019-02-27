caea0e70f6497a2d1880ac349b54f702688cef5b
Markus Hauge <markus.hauge@hotmail.com>
2019-02-28 00:22:35 +0100

    Refactor URL generation

--- packages/backend/lib/storage.mjs
+++ packages/backend/lib/storage.mjs
@@ -1,10 +1,11 @@
 import randomize from "randomatic";
 
 const storage = {};
+const generateUrl = key => `https://www.koble.jobs/${key}`;
 
 export const longToShort = longUrl => {
   const key = randomize("Aa0", 6);
-  const shortUrl = `https://www.koble.jobs/${key}`;
+  const shortUrl = generateUrl(key);
   storage[shortUrl] = longUrl;
   return shortUrl;
 };
